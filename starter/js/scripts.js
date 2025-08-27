1f76677c69694dcd6397b6ddd9f1e384952440e6
creatiko <creatiko@outlook.com>
2025-08-27 14:55:22 -0400

    Feat: Start work on the projects section

--- starter/js/scripts.js
+++ starter/js/scripts.js
@@ -14,7 +14,8 @@ const fetchUserData = async () => {
         const headshotContainer = document.createElement('div'); // create div element
         headshotContainer.classList.add('headshotContainer'); // add class to div element
         const headshotImage = document.createElement('img'); // create img element
-        headshotImage.src = userData.headshot; // populate the img
+        headshotImage.setAttribute('src', userData.headshot2); // populate the img
+        headshotImage.setAttribute('alt', 'Picture of the author'); // Set the alt text
         headshotContainer.append(headshotImage); // add it to the div
         const aboutDocumentFragment = document.createDocumentFragment(); // create fragment
         aboutDocumentFragment.append(bioContainer, headshotContainer); // add the p and the div
@@ -26,3 +27,33 @@ const fetchUserData = async () => {
 }
 
 fetchUserData();
+
+const fetchProjectsData = async () => {
+    try {
+        const responseData = await fetch('./data/projectsData.json');
+        if (!responseData.ok) {
+            throw new Error(`Error fetching data: ${responseData.status}, ${responseData.statusText}`);
+        }
+        const projectsData = await responseData.json();
+        console.log(projectsData);
+        const projectsContainer = document.getElementById("projectList"); // Main outside projects <sidebar>
+        // Loop through each project
+        projectsData.forEach(project => {
+            const projectCard = document.createElement('div'); // create div element
+            projectCard.setAttribute('id', project.project_id); // give each div an id that matches the project_id
+            projectCard.innerHTML = `
+                    <h2>${project.project_name}</h2>
+                    <p>${project.short_description || ""}</p>
+                    <img src="${project.card_image || ""}" alt="${project.project_name}">
+                    <a href="${project.url || '#'}" target="_blank">View Project</a>
+                `;
+            projectsContainer.append(projectCard);
+        });
+
+
+    } catch (error) {
+        console.error("Error loading JSON:", error);
+    }
+}
+
+fetchProjectsData();
